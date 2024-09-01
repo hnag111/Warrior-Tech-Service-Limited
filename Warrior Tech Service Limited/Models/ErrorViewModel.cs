042ec4201fbb85ae5c9feb03f8591b6d42918664
INFOCORP\harshit.nag <harshit.nag@spglobal.com>
2024-09-01 14:44:39 +0530

    Add project files.

---
+++ Warrior Tech Service Limited/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Warrior_Tech_Service_Limited.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
